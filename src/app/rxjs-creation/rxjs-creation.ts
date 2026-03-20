e1edb76c76c762c71b375815c7b55d7fe59fce2b
Harini Sparkout <harinisri@sparkouttech.com>
2026-03-20 18:58:18 +0530

    working in operators

--- src/app/rxjs-creation/rxjs-creation.ts
+++ src/app/rxjs-creation/rxjs-creation.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import {of,from,interval,Subscription,take,timer} from 'rxjs';
+import { Component, OnInit,ElementRef,ViewChild } from '@angular/core';
+import {of,from,interval,Subject,take,timer,fromEvent,takeUntil,map,filter,tap} from 'rxjs';
 @Component({
   selector: 'app-rxjs-creation',
   imports: [],
@@ -11,10 +11,14 @@ values :number []=[];
 fruits: string []=[];
 intervalvalues:number[]=[];
 timervalues:number[]=[];
- private subscriptions: Subscription[] = [];
+private destroy$ = new Subject<void>();
+// private subscriptions: Subscription[] = [];
+ clickCount =0;
+ @ViewChild ('btn',{static:true} ) btn !:  ElementRef <HTMLButtonElement>
 //emittedValue: any;
 ngOnInit(): void {
   const numbers$ = of (10,20,30);
+  const birds$ = of (['dove','sparrow','parrot'])
   numbers$.subscribe({
     next: value => this.values.push(value),
     complete : () => console.log('observable completed ')
@@ -33,31 +37,35 @@ ngOnInit(): void {
       complete: () => console.log('Observable completed.')
     });**/
   
-  const fruitsname = ['apple','orange','mango','pineapple','starfruit'];
-  const fruits$ = from(fruitsname)
-  fruits$.subscribe({
-     next : name => this.fruits.push(name),
-  complete : () => console.log('from observables')
-  })
-
-  
- const interval$ = interval(1000).pipe(
-  take(5)
+ const fruitsname = ['apple','orange','mango','pineapple','starfruit'];
+const fruits$ = from(fruitsname).pipe(
+  map(name => name.toUpperCase()),// used map for transformation without changing original  value
+  filter(name => name.length > 6)   //filter only give matched condition
 );
 
-  this.subscriptions.push(
+fruits$.subscribe({
+  next: name => this.fruits.push(name),
+  complete: () => console.log('from observable with map')
+});
+
+  //interval-immediate start and comes 
+ const interval$ = interval(1000).pipe(// without opreator we can use subscription to prevent memorry leakage 
+  take(5)// take() operator used to emit limited values
+);
+
+  //this.subscriptions.push(
   interval$.subscribe({
     next: time => {
       this.intervalvalues.push(time);
       console.log('interval ',time);
     },
     complete: () => console.log('interval completed')
-  })
+  } //)
 );
 
-
+//timer- make delay to start run
 const timer$ =timer(1000);
-this.subscriptions.push(
+
   timer$.subscribe({
     next: val => {
       this.timervalues.push(val);
@@ -65,12 +73,18 @@ this.subscriptions.push(
     },
     complete: () => console.log('timercompleted')
   })
-)
 
+//fromEvent - 
+fromEvent(this.btn.nativeElement, 'click').pipe(
+  takeUntil(this.destroy$),
+  tap(() => console.log('Button was clicked!')) // side effect using tap
+).subscribe(() => {
+  this.clickCount++;
+  console.log('button clicked', this.clickCount);
+});
 }
-ngOnDestroy(): void {
-    this.subscriptions.forEach(sub => sub.unsubscribe());
-    console.log('All subscriptions unsubscribed');
-  }
-
+ngOnDestroy() {
+  this.destroy$.next();
+  this.destroy$.complete();
+}
 }
