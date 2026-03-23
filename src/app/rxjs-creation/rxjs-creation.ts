4f565fce1c62dcbb67c657bf91c1036a2a29bce2
Harini Sparkout <harinisri@sparkouttech.com>
2026-03-23 18:48:18 +0530

    operators

--- src/app/rxjs-creation/rxjs-creation.ts
+++ src/app/rxjs-creation/rxjs-creation.ts
@@ -1,30 +1,40 @@
-import { Component, OnInit,ElementRef,ViewChild } from '@angular/core';
-import {of,from,interval,Subject,take,timer,fromEvent,takeUntil,map,filter,tap} from 'rxjs';
+import { CommonModule, JsonPipe } from '@angular/common';
+import { Component, OnInit,ElementRef,ViewChild,AfterViewInit } from '@angular/core';
+import {of,from,interval,Subject,take,timer,fromEvent,takeUntil,map,filter,tap,switchMap, merge, combineLatest, forkJoin,
+  withLatestFrom, delay} from 'rxjs';
+ 
 @Component({
   selector: 'app-rxjs-creation',
-  imports: [],
+  standalone:true,
+  imports: [JsonPipe,CommonModule],
   templateUrl: './rxjs-creation.html',
   styleUrl: './rxjs-creation.css',
 })
-export class RxjsCreation implements OnInit{
+export class RxjsCreation implements OnInit,AfterViewInit{
 values :number []=[];
 fruits: string []=[];
 intervalvalues:number[]=[];
 timervalues:number[]=[];
+mergedValues: any[] = [];
+combinedValues: any[] = [];
+forkJoinValues: any[] = [];
+latestValues: any[] = [];
 private destroy$ = new Subject<void>();
 // private subscriptions: Subscription[] = [];
  clickCount =0;
- @ViewChild ('btn',{static:true} ) btn !:  ElementRef <HTMLButtonElement>
+@ViewChild('btn', { static: true }) btn!: ElementRef<HTMLButtonElement>;
 //emittedValue: any;
 ngOnInit(): void {
   const numbers$ = of (10,20,30);
-  const birds$ = of (['dove','sparrow','parrot'])
+  //const birds$ = of (['dove','sparrow','parrot'])
   numbers$.subscribe({
     next: value => this.values.push(value),
     complete : () => console.log('observable completed ')
 
 
   })
+
+  
   //of using array 
 
   /* * const array$ = of([1, 2, 3]);
@@ -73,15 +83,47 @@ const timer$ =timer(1000);
     },
     complete: () => console.log('timercompleted')
   })
+  const merged$ = merge(numbers$, interval$.pipe(delay(500)));
 
-//fromEvent - 
-fromEvent(this.btn.nativeElement, 'click').pipe(
-  takeUntil(this.destroy$),
-  tap(() => console.log('Button was clicked!')) // side effect using tap
-).subscribe(() => {
-  this.clickCount++;
-  console.log('button clicked', this.clickCount);
+merged$.subscribe(val => {
+  this.mergedValues.push(val);
+  console.log('merge:', val);
 });
+ //combineLatest
+    const combined$ = combineLatest([
+      numbers$,
+      interval$.pipe(delay(500))
+    ]);
+
+    combined$.subscribe(val => {
+      this.combinedValues.push(val);
+      console.log('combineLatest:', val);
+    });
+
+    //forkJoin (waits for all to complete)
+    const fork$ = forkJoin([
+      numbers$,
+      fruits$.pipe(delay(500))
+    ]);
+
+    fork$.subscribe(val => {
+      this.forkJoinValues.push(val);
+      console.log('forkJoin:', val);
+    });
+
+
+}
+  ngAfterViewInit(): void {
+  fromEvent(this.btn.nativeElement, 'click').pipe(
+    takeUntil(this.destroy$),
+    tap(() => console.log('Button was clicked!')),
+    switchMap(() => interval(1000).pipe(take(3))),
+    withLatestFrom(of(10,20,30))
+  ).subscribe(([intervalVal, numberVal]) => {
+    this.clickCount++;
+    this.latestValues.push({ intervalVal, numberVal });
+    console.log('switchMap + withLatestFrom:', intervalVal, numberVal);
+  });
 }
 ngOnDestroy() {
   this.destroy$.next();
